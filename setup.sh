fc2058037e8b6fadedacfec00bb3a613a5c6d3f7
andrewtvuong <15879426+andrewtvuong@users.noreply.github.com>
2025-03-17 20:28:01 -0700

    update

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt-get update && sudo apt-get install vim -y && sudo apt-get install zsh -y && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
